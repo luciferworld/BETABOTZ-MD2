e1715e38f56d1dbea52a3c91e10ade76701689bd
Ibrahim khan <108752662+luciferworld@users.noreply.github.com>
2024-01-11 18:20:48 +0500

    Update owner-addprem.js

--- plugins/owner-addprem.js
+++ plugins/owner-addprem.js
@@ -3,16 +3,16 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
     let user = db.data.users[who]
-    if (!who) throw `tag atau balas pesan yang mau dijadikan premium!`
+    if (!who) throw `tag or reply to messages that you want to make premium!`
     let txt = text.replace('@' + who.split`@`[0], '').trim()
-    if (!txt) throw `berapa hari mamaskuh?`
-    if (isNaN(txt)) return m.reply(`hanya nomor mamaskuh!\n\ncontoh:\n${usedPrefix + command} @${m.sender.split`@`[0]} 7`)
+    if (!txt) throw `how many days mamasuh?`
+    if (isNaN(txt)) return m.reply(`just mamasuh number!\n\nexample:\n${usedPrefix + command} @${m.sender.split`@`[0]} 7`)
     var jumlahHari = 86400000 * txt
     var now = new Date() * 1
     if (now < user.premiumTime) user.premiumTime += jumlahHari
     else user.premiumTime = now + jumlahHari
     user.premium = true
-    m.reply(`Berhasil!\n*${user.name}* sekarang sudah premium ${txt} hari.\n\ncountdown: ${conn.msToDate(user.premiumTime - now)}`)
+    m.reply(`Berhasil!\n*${user.name}* now it's premium ${txt} hari.\n\ncountdown: ${conn.msToDate(user.premiumTime - now)}`)
 }
 handler.help = ['addprem [@user] <hari>']
 handler.tags = ['owner']
