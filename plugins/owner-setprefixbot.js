ba2d6bb1c2f8daf72cefda19e3eefca9b992882b
Ibrahim khan <108752662+luciferworld@users.noreply.github.com>
2024-01-11 18:26:41 +0500

    Update owner-setprefixbot.js

--- plugins/owner-setprefixbot.js
+++ plugins/owner-setprefixbot.js
@@ -1,7 +1,7 @@
 let handler = async(m, { conn, text }) => {
   if (!text) throw `No Prefix detected...`
   global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
-  await m.reply(`Prefix telah ditukar ke *${text}*`)
+  await m.reply(`The prefix has been changed to *${text}*`)
     // conn.fakeReply(m.chat, 'Prefix telah ditukar ke *${text}*', '0@s.whatsapp.net', 'Set Prefix Bot')
 }
 handler.help = ['setprefix'].map(v => v + ' [prefix]')
