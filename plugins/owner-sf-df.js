81e4d448ecf5cfd481f020da282de28db6b9c619
Ibrahim khan <108752662+luciferworld@users.noreply.github.com>
2024-01-11 18:29:27 +0500

    Update owner-sf-df.js

--- plugins/owner-sf-df.js
+++ plugins/owner-sf-df.js
@@ -2,27 +2,27 @@ let fs = require('fs');
 
 let handler = async (m, { text, usedPrefix, command }) => {
 
-  if (!text) throw `uhm.. teksnya mana?\n\npenggunaan:\n${usedPrefix + command} <teks>\n\ncontoh:\n${usedPrefix + command} menu`;
+  if (!text) throw `uhm.. where's the text?\n\nusage:\n${usedPrefix + command} <teks>\n\nexample:\n${usedPrefix + command} menu`;
 
   if (command === 'sf') {
 
-    if (!m.quoted.text) throw `balas pesan nya!`;
+    if (!m.quoted.text) throw `reply to the message!`;
 
     let path = `plugins/${text}.js`;
 
     await fs.writeFileSync(path, m.quoted.text);
 
-    m.reply(`tersimpan di ${path}`);
+    m.reply(`saved in ${path}`);
 
   } else if (command === 'df') {
 
     let path = `plugins/${text}.js`;
 
-    if (!fs.existsSync(path)) throw `file plugin ${text}.js tidak ditemukan`;
+    if (!fs.existsSync(path)) throw `file plugin ${text}.js not found`;
 
     fs.unlinkSync(path);
 
-    m.reply(`file plugin ${text}.js berhasil dihapus`);
+    m.reply(`file plugin ${text}.js deleted successfully`);
 
   }
 
